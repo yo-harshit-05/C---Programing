6797e8a2ae603d4198c522ba4b3793a45ecf0625
Harshit Kumar <harshit24164048@akgec.ac.in>
2025-02-28 23:37:25 +0530

    04temp.c

--- 04temp.c
+++ 04temp.c
@@ -1,4 +1,4 @@
-// Progam to transform temperature in celsis to farenheit 
+// Program to transform temperature in celsis to farenheit 
 #include <stdio.h>
 int main(){
     float c,f;
@@ -7,4 +7,18 @@ int main(){
     f=(9*c)/5+32;
     printf("Fahrenheit Temperature is \n %f",f);
     return 0;
+}
+// Program to transform temperature in celsis to farenheit using function
+#include<stdio.h>
+float temp(float c){
+    float f;
+    f=(9*c)/5+32;
+    printf("Fahrenheit Temperature is \n %f",f);
+    return f;
+}
+int main(){
+    float c;
+    printf("Enter the celsius value\n");
+    scanf("%f",&c);
+    temp(c);
 }
