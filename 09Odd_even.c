bdcfcb1c89b71a63d7fcbc810b2d16f2802b4638
Harshit Kumar <harshit24164048@akgec.ac.in>
2025-04-23 22:58:28 +0530

    Create 09Odd_even.c

---
+++ 09Odd_even.c
@@ -0,0 +1,15 @@
+// Program 10 : To check a number is odd or even
+#include<stdio.h>
+void main(){
+    int num;
+    printf("Enter any number\n");
+    scanf("%d",&num);
+    if (num%2==0)
+    {
+        printf("The given number is even number\n");
+    }
+    else
+    {
+        printf("The given number is odd\n");
+    }
+}
